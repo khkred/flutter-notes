5eb648df1de4d421a6e4b86ef70256d63690a647
khkr <khkr>
2022-01-28 15:12:31 +0530

    Mini project V1

---
+++ flutter-programs/first_project/lib/sample.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Person
+{
+  String name = "aaa";
+  int age = 20;
+  String position = "sample position";
+
+  Person(this.name, this.age, this.position);
+
+}

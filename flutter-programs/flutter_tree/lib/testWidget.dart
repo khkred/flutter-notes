d9dc92588fc6b1253fed0d77c30ecb054beb74a1
khkr <32921476+khkred@users.noreply.github.com>
2022-02-08 15:13:38 +0530

    Quiz and results Widgets

---
+++ flutter-programs/flutter_tree/lib/testWidget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatelessWidget {
+
+  List<Map<String,Object>> sampleList;
+
+  TestWidget(this.sampleList);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

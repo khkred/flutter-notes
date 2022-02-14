4d5239502d0c5a66e399c49ae3859658445e38a0
khkr <32921476+khkred@users.noreply.github.com>
2022-02-14 14:42:43 +0530

    Added Two cards
    First Card: Chart of our expenses.
    
    Second Card: Transaction.

--- flutter-programs/expense_planner/lib/main.dart
+++ flutter-programs/expense_planner/lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Expense"),),
-      body: Center(
-        child: Text('This is the expense planner'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Budget Planner"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.red,
+                child: Text("Chart"),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              color: Colors.amber,
+              child: Text("Transaction"),
+            ),
+          ],
+        ));
   }
 }

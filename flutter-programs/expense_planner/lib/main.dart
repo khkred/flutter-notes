e1e0963332ad62bbf7935af488dd1c352d869150
Aarav <aaravputtnam04@gmail.com>
2022-02-14 15:03:14 +0530

    added 2 new transactions

--- flutter-programs/expense_planner/lib/main.dart
+++ flutter-programs/expense_planner/lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
 
   List<Transaction> transactionsList = [
     Transaction(id: "t1", title: "Shoes", amount: 108.6, date: DateTime.now()),
-    Transaction(id: "t2", title: "milk", amount: 30, date: DateTime.now())
+    Transaction(id: "t2", title: "milk", amount: 30, date: DateTime.now()),
+    Transaction(id: "t3", title: "Console", amount: 10000 , date: DateTime.now()),
+    Transaction(id: "id", title: "PC", amount: 50000, date: DateTime.now())
   ];
   @override
   Widget build(BuildContext context) {

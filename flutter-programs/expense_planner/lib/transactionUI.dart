08c511145873843ed53434fb3b58b3a8ba39fcb1
Aarav <aaravputtnam04@gmail.com>
2022-02-15 15:09:34 +0530

    added transactionui

---
+++ flutter-programs/expense_planner/lib/transactionUI.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TransactionUI extends StatelessWidget {
+  final txn;
+
+  TransactionUI(this.txn);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        //First Child
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all()
+          ),
+          child: Text(
+            txn.amount.toString(),
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.purple),
+          ),
+          margin: EdgeInsets.symmetric(
+              vertical: 10, horizontal: 15),
+          padding: EdgeInsets.all(10),
+        ),
+        // Second child is a column
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              txn.title,
+              style: TextStyle(fontSize: 15),
+            ),
+            Text(
+              txn.date.toString(),
+              style: TextStyle(
+                  color: Colors.grey, fontSize: 10),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
--- flutter-programs/expense_planner/lib/transactions_widget_list.dart
+++ flutter-programs/expense_planner/lib/transactions_widget_list.dart
@@ -1,5 +1,7 @@
+import 'package:expense_planner/transactionUI.dart';
 import 'package:flutter/material.dart';
 import 'transaction.dart';
+import 'transactionUI.dart';
 
 class TransactionsWidgetList extends StatelessWidget {
 
@@ -12,41 +14,7 @@ class TransactionsWidgetList extends StatelessWidget {
     return Column(
       children: transactionsList
           .map((txn) => Card(
-        child: Row(
-          children: [
-            //First Child
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all()
-              ),
-              child: Text(
-                txn.amount.toString(),
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-              margin: EdgeInsets.symmetric(
-                  vertical: 10, horizontal: 15),
-              padding: EdgeInsets.all(10),
-            ),
-            // Second child is a column
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  txn.title,
-                  style: TextStyle(fontSize: 15),
-                ),
-                Text(
-                  txn.date.toString(),
-                  style: TextStyle(
-                      color: Colors.grey, fontSize: 10),
-                )
-              ],
-            )
-          ],
-        ),
+        child: TransactionUI(txn),
       ))
           .toList(),
     );

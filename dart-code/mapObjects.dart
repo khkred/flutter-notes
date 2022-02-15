9a304c73c194b88e9498d10d8a26455d71f9cd39
khkr <32921476+khkred@users.noreply.github.com>
2022-02-15 13:53:15 +0530

    mapObjects solution

--- dart-code/mapObjects.dart
+++ dart-code/mapObjects.dart
@@ -1,27 +1,36 @@
 void main() {
-  //TODO 1: Create an arrayList of type Person, with 10 objects.
-  final peopleList = const[
-    Person["Aarav",14];
-    Person["Michael",8];
-    Person["Franklin",8];
-    Person["Trevor",8];
-    Person["CJ",17];
-    Person["Kratos",4];
-    Person["Jasmine",14];
-    Person["David",14];
-    Person["Steve",12];
-    Person["Jay",10];
-    Person["Kai",10];
-    Person["Mikey",30];
-  ]
+  //Completed 1: Create an arrayList of type Person, with 10 objects.
+
+  List peopleList = [
+    Person("Aarav", 14),
+    Person("Michael", 8),
+    Person("Franklin", 8),
+    Person("Trevor", 8),
+    Person("CJ", 17),
+    Person("Kratos", 23),
+    Person("Jasmine", 14),
+    Person("David", 14),
+    Person("Steve", 12),
+    Person("Jay", 10),
+    Person("Kai", 10),
+    Person("Mikey", 30),
+  ];
 
   //TODO 2: Using the map method print the name of the person using print()
 
+//   for(var person in peopleList)
+//   {
+//     print(person.name);
+//   }
+
+  var namesList = peopleList.map((person) {
+    return person.name;
+  }).toList();
+
+  print(namesList);
 }
 
 class Person {
-  final List<Map<String,Object>> peopleList;
-
   String name;
   int age;
   Person(this.name, this.age);

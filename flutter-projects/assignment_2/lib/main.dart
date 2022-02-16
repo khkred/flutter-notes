2775c2d4d84a15885fe9b3d4fcba29a65c55770f
khkr <32921476+khkred@users.noreply.github.com>
2022-02-16 13:56:57 +0530

    corrections to assignment-2

--- flutter-projects/assignment_2/lib/main.dart
+++ flutter-projects/assignment_2/lib/main.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //Every App needs a MaterialApp in main.dart
+  
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Row(
-            children: [],
+    
+    //The build method inside main.dart needs scaffold
+    return Scaffold(
+      appBar: AppBar(title: Text("Assignment 2"),),
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [Text("First Child"),
+              Text(("Second Child"))],
+            ),
           ),
-        ),
-        Container(
-            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
-            padding: EdgeInsets.all(10),
-            child: Card()
-        ),
-        Container(
-          child: Text("This is assignment 2", style: TextStyle(fontSize: 30),),
-        )
-      ],
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+              padding: EdgeInsets.all(10),
+              child: Card(
+                child: Text("This is a card"),
+              )
+          ),
+          Container(
+            child: Text("This is assignment 2", style: TextStyle(fontSize: 30),),
+          )
+        ],
+      ),
     );
   }
 }

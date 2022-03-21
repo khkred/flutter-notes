834935bc9a6bc07fc2b94921d2afc8e8ef6c3dba
khkr <32921476+khkred@users.noreply.github.com>
2022-03-21 15:10:15 +0530

    added images

--- harish-programs/functions/lib/main.dart
+++ harish-programs/functions/lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("Images"),
       ),
-      body: const Image(
-        image: NetworkImage('https://images.unsplash.com/photo-1629809189194-8302d4345c8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'),
-      ),
+      body:  Image.asset('images/franklin.jpg'),
     );
   }
 }

36fb64c2901265a693ee60f41af34ad53b202ee8
khkr <32921476+khkred@users.noreply.github.com>
2022-03-24 18:57:00 +0530

    created gridview image picker

---
+++ harish-programs/gridview_image_picker/lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+
+  int counter = 0;
+  File _imageFile;
+  List<File> _imageFileList = [];
+
+  getImage() async {
+    var fileName = 
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker'),
+      ),
+
+      body: ,
+    );
+  }
+}

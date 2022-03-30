c021a4c33a2850454fa3b9dc56ac9604a3c2dd7a
khkr <32921476+khkred@users.noreply.github.com>
2022-03-30 14:56:06 +0530

    Book class Created

--- harish-programs/books_app/lib/model/book.dart
+++ harish-programs/books_app/lib/model/book.dart
@@ -1,7 +1,12 @@
-class Book{
+class Book {
   String name;
   String author;
   String description;
 
-  Book({required this.name,required this.author,required this.description});
+  Book({required this.name, required this.author, required this.description});
+
+  factory Book.fromJson(Map<String, dynamic> book_json) => Book(
+      name: book_json["name"],
+      author: book_json["author"],
+      description: book_json["description"]);
 }

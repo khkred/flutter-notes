70a735ba0ed3b77d842d3c8852f9f446d4e16d7a
aaravprimeYT <aaravputtnam04@gmail.com>
2022-04-15 09:21:48 +0530

    tv episode app

---
+++ aarav-projects/tv_serial/android/app/src/main/kotlin/com/example/tv_serial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tv_serial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aarav-projects/tv_serial/lib/tv_network.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart' as http;
+
+class Download{
+  String baseUrl;
+
+  Download(this.baseUrl);
+  Future<String> getResponseFromApi() async{
+    //String url = "https://api.themoviedb.org/3/movie/popular?api_key=f5bfcf47f66210e0ffe754d19174d7c2&language=en-US&page=1";
+
+    var _apiKey = "api_key=f5bfcf47f66210e0ffe754d19174d7c2";
+
+    String trailingUrl = "&language=en-US";
+
+    String url = baseUrl + _apiKey + trailingUrl;
+
+    var uri_1 = Uri.parse(url);
+
+    var client = http.Client();
+
+    var response = await client.get(uri_1);
+
+    return response.body;
+  }
+
+
+}

b119833bca32835ba3ab9d8e718ca15cb06312fe
aaravprimeYT <aaravputtnam04@gmail.com>
2022-04-21 17:33:03 +0530

    containers

--- harish-programs/container_ui/lib/main.dart
+++ harish-programs/container_ui/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-
 
   String imageUrl = "https://images.unsplash.com/photo-1647950440643-6dfbeaff0c25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80";
 
@@ -18,29 +17,32 @@ class MyApp extends StatelessWidget {
       body: Center(
         child: Container(
 
-          height: 200,
-          width: 150,
-          decoration:  BoxDecoration(
+          height: 300,
+          width: 200,
+          decoration: BoxDecoration(
 
-            image: DecorationImage(
-              image: NetworkImage(imageUrl),
+              image: DecorationImage(
+                image: NetworkImage(imageUrl),
 
-            ),
-            border: Border.all(color: Colors.greenAccent,
-            width: 4,
-            style: BorderStyle.solid)
+              ),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(60),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)
+              ),
+              border: Border.all(color: Colors.greenAccent,
+                  width: 2,
+                  style: BorderStyle.solid),
 
-            // boxShadow: [
-            //   BoxShadow(
-            //     blurRadius: 20.0,
-            //     spreadRadius: 15,
-            //     offset: const Offset(5.0,8.0),
-            //     color: Colors.redAccent.withOpacity(0.3)
-            //   ),
-            // ]
-          ),
+              boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: Offset
+              ),
+          ],
         ),
       ),
-    );
+    ),);
   }
 }

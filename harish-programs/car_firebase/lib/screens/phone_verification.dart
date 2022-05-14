fc83626eb332f68fe43c36e4934de70311c9bf61
khkr <32921476+khkred@users.noreply.github.com>
2022-05-14 08:42:33 +0530

    Added UI for phone_verification

--- harish-programs/car_firebase/lib/screens/phone_verification.dart
+++ harish-programs/car_firebase/lib/screens/phone_verification.dart
@@ -8,8 +8,79 @@ class PhoneVerification extends StatefulWidget {
 }
 
 class _PhoneVerificationState extends State<PhoneVerification> {
+  final _phoneController = TextEditingController();
+  final _passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(32),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Login",
+              style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey.shade200)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey.shade300)),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  hintText: "Phone Number"),
+              controller: _phoneController,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey.shade200)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey.shade300)),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  hintText: "Password"),
+              controller: _passController,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  padding: EdgeInsets.all(16),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                child: const Text("Login"),
+                //padding: EdgeInsets.all(16),
+                onPressed: () {
+                  //code for sign in
+                },
+                // color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

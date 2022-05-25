bd8d9a5da19f5f8eba563425d3f77e3b91fd6a86
khkr <32921476+khkred@users.noreply.github.com>
2022-05-25 08:55:02 +0530

    on Back pressed implemented in home_screen.dart

--- harish-programs/car_firebase/lib/screens/dynamic_user_validation.dart
+++ harish-programs/car_firebase/lib/screens/dynamic_user_validation.dart
@@ -48,40 +48,66 @@ class _DynamicUserValidationState extends State<DynamicUserValidation> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        width: double.infinity,
-        height: double.infinity,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 3,
-              child: TextFormField(
-                decoration: const InputDecoration(hintText: "Enter your email"),
-                controller: myController,
-              ),
-            ),
-            Expanded(
-                flex: 1,child: IconButton(onPressed: () {
-                  setState(() {
-                    _email = myController.text;
-                  });
-                  if(_email.isEmpty){
-                    const snackBar = SnackBar(
-                      content: Text("Email is empty"),
-                    );
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(snackBar);
-                  } else {
-                    validateUser(_email);
-                  }
+    return WillPopScope(
+      onWillPop: () async{
+        print("Back pressed");
+        var result = await _onBackPressed(context);
 
-            }, icon: const Icon(Icons.arrow_forward))),
-          ],
+        /**
+         * If the result is null, return false
+         */
+        return result ?? false;
+      },
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          width: double.infinity,
+          height: double.infinity,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 3,
+                child: TextFormField(
+                  decoration: const InputDecoration(hintText: "Enter your email"),
+                  controller: myController,
+                ),
+              ),
+              Expanded(
+                  flex: 1,child: IconButton(onPressed: () {
+                    setState(() {
+                      _email = myController.text;
+                    });
+                    if(_email.isEmpty){
+                      const snackBar = SnackBar(
+                        content: Text("Email is empty"),
+                      );
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(snackBar);
+                    } else {
+                      validateUser(_email);
+                    }
+
+              }, icon: const Icon(Icons.arrow_forward))),
+            ],
+          ),
         ),
       ),
     );
   }
+
+ Future<bool?> _onBackPressed(BuildContext context) {
+
+    // show Dialog will return the boolean value
+
+   return showDialog<bool>(context: context, builder: (context)=>AlertDialog(
+     content: Text("Are you sure?"),
+     title: Text("Do you want to exit"),
+
+     actions: [
+       ElevatedButton(onPressed: ()=>Navigator.pop(context,false), child: Text("No")),
+       ElevatedButton(onPressed: ()=>Navigator.pop(context,true), child: Text("Yes"))
+     ],
+   ));
+ }
 }
--- harish-programs/car_firebase/lib/screens/home_screen.dart
+++ harish-programs/car_firebase/lib/screens/home_screen.dart
@@ -18,59 +18,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
 
-  //  Future<bool> _onBackPressed(){
-  //   return showDialog(context: context, builder: (context){
-  //     return AlertDialog(
-  //       title: const Text('Are you Sure?'),
-  //       content: const Text('Do you want to logout?'),
-  //       actions: [
-  //         GestureDetector(
-  //           child: const Text('No'),
-  //           onTap: (){
-  //             Navigator.of(context).pop(false);
-  //           },
-  //         ),
-  //        const SizedBox(height: 16),
-  //         GestureDetector(
-  //           child: const Text('Yes'),
-  //           onTap: (){
-  //             //Signout the user
-  //             widget.auth.signOut();
-  //             Navigator.of(context).pop(true);
-  //           },
-  //         )
-  //       ],
-  //     );
-  //   }) ?? false;
-  // }
-
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [IconButton(onPressed: (){
-          signOutUser();
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const DynamicUserValidation()));
-        }, icon: const Icon(Icons.exit_to_app))],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("You are Logged in succesfully", style: TextStyle(color: Colors.lightBlue, fontSize: 32),),
-            SizedBox(height: 16,),
-            Text("${widget.user.email} is signed in",style: TextStyle(color: Colors.grey, ),),
-            Text("${widget.user.phoneNumber}", style: TextStyle(color: Colors.grey, ),),
+    return WillPopScope(
+      onWillPop: () async{
+        print("Back pressed");
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [IconButton(onPressed: (){
+            signOutUser();
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const DynamicUserValidation()));
+          }, icon: const Icon(Icons.exit_to_app))],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("You are Logged in succesfully", style: TextStyle(color: Colors.lightBlue, fontSize: 32),),
+              SizedBox(height: 16,),
+              Text("${widget.user.email} is signed in",style: TextStyle(color: Colors.grey, ),),
+              Text("${widget.user.phoneNumber}", style: TextStyle(color: Colors.grey, ),),
 
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ListPets()));
-            }, child: Text("List of Pets"))
+              ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ListPets()));
+              }, child: Text("List of Pets"))
 
-            //TODO: Add a button with label "list of pets"
-          ],
+              //TODO: Add a button with label "list of pets"
+            ],
+          ),
         ),
       ),
     );

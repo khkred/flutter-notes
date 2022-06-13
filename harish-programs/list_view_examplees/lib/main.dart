746cc98b5b64b84125128082802d64991f7343ec
khkr <32921476+khkred@users.noreply.github.com>
2022-06-13 07:25:58 +0530

    changes in listview code

--- harish-programs/list_view_examplees/lib/main.dart
+++ harish-programs/list_view_examplees/lib/main.dart
@@ -11,23 +11,21 @@ class MyApp extends StatelessWidget {
 
   //generate 10 persons
 
-  var persons = List<String>.generate(20, (index) => 'person ${index+1}');
+  var persons = List<String>.generate(1000, (index) => 'person ${index+1}');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('List View'),),
-      body: Column(
-        children: [
-          Text('Here is the List'),
+      body: ListView.separated(itemBuilder: (context, int index){
+        return ListTile(title: Text(persons[index]),);
+      }, separatorBuilder: (context,int index){
+        return Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(2),
+            child: const Text('------------------------'));
+      }, itemCount: persons.length)
 
-          Container(
-            height: 500,
-            child: ListView(
-              children: persons.map((item) => ListTile(title: Text(item),)).toList(),
-            ),
-          )
-        ],
-      ),
+
     );
 
   }

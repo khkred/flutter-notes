813381441b2c79c0813827ab3c9d0fa22a42c1ee
aaravprimeYT <aaravputtnam04@gmail.com>
2022-06-21 19:31:26 +0530

    maps

---
+++ aarav-projects/comments_assignment/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: MyApp(),
+  ))
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  void getComments(){
+    final response =
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

3b04d06b7d18129d770bf8f867812726ad69bf0e
aaravprimeYT <aaravputtnam04@gmail.com>
2022-07-06 19:28:36 +0530

    vetcard

---
+++ maps-apps/final_nearby_vets/lib/vetCard.dart
@@ -0,0 +1,25 @@
+import 'package:final_nearby_vets/models/vet_clinic.dart';
+import 'package:flutter/material.dart';
+
+class VetCard extends StatelessWidget {
+
+  VetClinic vetClinic;
+
+  VetCard({required this.vetClinic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vet List"),
+      ),
+      body: Column(
+        children: [
+          Card(
+          child: ,
+      );
+        ],
+      ),
+    );
+  }
+}
